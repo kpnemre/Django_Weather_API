f2cc2c1ece0203e9913768062526e14bb37d4566
kpnemre <kpnemre51@gmail.com>
2021-01-11 23:21:39 +0300

    Add apikey and index file

--- src/weather/views.py
+++ src/weather/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from decouple import config
+import requests
 
 # Create your views here.
 
 def index(request):
+    url = config("BASE_URL")
+    city ="Berlin"
+    r = requests.get(url.format(city))
+    print(r)
     return render(request, "weather/index.html")

b9cdcc1171a5107c423afcb575c79847d218d51d
kpnemre <kpnemre51@gmail.com>
2021-01-12 12:06:16 +0300

    Add model and admin register

--- src/weather/views.py
+++ src/weather/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from decouple import config
 import requests
-
+from pprint import pprint
+from .models import City
 # Create your views here.
 
 def index(request):
+    cities = City.objects.all()
     url = config("BASE_URL")
     city ="Berlin"
     r = requests.get(url.format(city))
-    print(r)
-    return render(request, "weather/index.html")
+    content=r.json()
+    # print(type(a))
+    # pprint(a)
+    context ={
+        'city':city,
+        'temp':content["main"]["temp"],
+        "description":content["weather"][0]["description"],
+        "icon":content["weather"][0]["icon"]
+        
+        
+    }
+    return render(request, "weather/index.html", context)

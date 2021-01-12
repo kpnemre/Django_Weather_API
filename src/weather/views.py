122c1b0a219602554011aa2504b12637896bea66
kpnemre <kpnemre51@gmail.com>
2021-01-12 15:16:08 +0300

    Add boostrap style

--- src/weather/views.py
+++ src/weather/views.py
@@ -38,6 +38,7 @@ def index(request):
         r = requests.get(url.format(city))
         # print(r.status_code)
         content=r.json()
+        pprint(content)
         
         weather_data ={
             'city':city.name,
@@ -53,4 +54,30 @@ def index(request):
             'city_data':city_data,
             'form':form,
        }
-    return render(request, "weather/index.html", context)
+    return render(request, "weather/index2.html", context)
+
+
+# {'base': 'stations',
+#  'clouds': {'all': 81},
+#  'cod': 200,
+#  'coord': {'lat': 37.8333, 'lon': 34.75},
+#  'dt': 1610451975,
+#  'id': 303826,
+#  'main': {'feels_like': 9.64,
+#           'grnd_level': 849,
+#           'humidity': 51,
+#           'pressure': 1017,
+#           'sea_level': 1017,
+#           'temp': 13.74,
+#           'temp_max': 13.74,
+#           'temp_min': 13.74},
+#  'name': 'Niğde',
+#  'sys': {'country': 'TR', 'sunrise': 1610427365, 'sunset': 1610462526},
+#  'timezone': 10800,
+#  'visibility': 10000,
+#  'weather': [{'description': 'parçalı bulutlu',
+#               'icon': '04d',
+#               'id': 803,
+#               'main': 'Clouds'}],
+#  'wind': {'deg': 159, 'speed': 3.91}}
+
